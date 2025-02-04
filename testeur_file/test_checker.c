0fa9b851bddf1b07d56342cc35bdbec2d2d2357f
Kamel Houmouad <kahoumou@bess-f3r6s5.clusters.42paris.fr>
2025-02-04 14:26:24 +0100

    bulding checker

--- testeur_file/test_checker.c
+++ testeur_file/test_checker.c
@@ -6,15 +6,13 @@
 /*   By: kahoumou <kahoumou@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/02/03 14:54:13 by kahoumou          #+#    #+#             */
-/*   Updated: 2025/02/03 15:40:37 by kahoumou         ###   ########.fr       */
+/*   Updated: 2025/02/04 14:24:34 by kahoumou         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
-
 
 #include "../headers/testeur.h"
 #include <stdbool.h>
 #include <stdio.h>
-
 
 void	run_test(char *filename, bool expected)
 {
@@ -29,16 +27,17 @@ void	run_test(char *filename, bool expected)
 		printf("\033[1;32m[SUCCÈS] Test réussi pour: %s\033[0m\n", filename);
 }
 
-int	main(void)
+int	main(int argc, char *argv[])
 {
+	(void)argc;
 	printf("=== Début des tests ===\n\n");
-	run_test("map_valid.cub", true);
-	run_test("map_invalid.txt", false);
-	run_test("image.xpm", false);
-	run_test("inexistant.cub", false);
-	run_test("map_sans_extension", false);
-	run_test(".cub", false);
-	run_test("map_invalid.cub.txt", false);
+	run_test(argv[1], true);
+	// run_test("map_valid.txt", true);
+	// run_test("image.xpm", false);
+	// run_test("inexistant.cub", false);
+	// run_test("map_sans_extension", false);
+	// run_test(".cub", false);
+	// run_test("map_invalid.cub.txt", false);
 	printf("\n=== Fin des tests ===\n");
 	return (0);
 }
