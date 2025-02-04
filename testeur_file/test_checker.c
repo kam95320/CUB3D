1baff2aa25e634735de7fa7aef427bca6ee69eda
Kamel Houmouad <kahoumou@bess-f3r6s5.clusters.42paris.fr>
2025-02-04 11:14:45 +0100

    builting testeur

---
+++ testeur_file/test_checker.c
@@ -0,0 +1,44 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   test_checker.c                                     :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: kahoumou <kahoumou@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/02/03 14:54:13 by kahoumou          #+#    #+#             */
+/*   Updated: 2025/02/03 15:40:37 by kahoumou         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+
+#include "../headers/testeur.h"
+#include <stdbool.h>
+#include <stdio.h>
+
+
+void	run_test(char *filename, bool expected)
+{
+	bool	result;
+
+	result = check_is_valid(filename);
+	printf("Test avec fichier: '%s' -> %s (attendu: %s)\n", filename,
+		result ? "VALIDE" : "INVALIDE", expected ? "VALIDE" : "INVALIDE");
+	if (result != expected)
+		printf("\033[1;31m[ÉCHEC] Test échoué pour: %s\033[0m\n", filename);
+	else
+		printf("\033[1;32m[SUCCÈS] Test réussi pour: %s\033[0m\n", filename);
+}
+
+int	main(void)
+{
+	printf("=== Début des tests ===\n\n");
+	run_test("map_valid.cub", true);
+	run_test("map_invalid.txt", false);
+	run_test("image.xpm", false);
+	run_test("inexistant.cub", false);
+	run_test("map_sans_extension", false);
+	run_test(".cub", false);
+	run_test("map_invalid.cub.txt", false);
+	printf("\n=== Fin des tests ===\n");
+	return (0);
+}
